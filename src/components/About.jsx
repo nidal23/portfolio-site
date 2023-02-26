af1c5681075a9faacf49672e9be76a6322dae945
Nidal Mohammed <nidalmo23@gmail.com>
2023-02-26 15:36:36 +0530

    changes made

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,33 +4,29 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white "
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500 sm:mt-3">
             About
           </p>
         </div>
 
-        <p className="text-xl mt-15 md:mb-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-          iste, totam fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
+        <p className="text-xl mt-18">
+          I'm a front-end developer with one year of experience building
+          websites for startups at Neuwave Labs. During this time, I honed my
+          skills in creating custom websites that met the unique needs of each
+          client. I've also spent the past three years working on various
+          projects that showcase my technical abilities and attention to detail.
         </p>
 
         <br />
 
-        <p className="text-xl md:mb-6">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          pariatur, vel similique sint, nobis aspernatur ut praesentium
-          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
-          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-          magni quo! Eum cupiditate debitis labore.
+        <p className="text-xl ">
+          When I'm not coding, I enjoy traveling to new places and exploring
+          different cultures. I'm also an avid Manchester United fan and enjoy
+          watching matches in my spare time. GGMU.
         </p>
       </div>
     </div>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -40,11 +40,14 @@ const Portfolio = () => {
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Portfolio
           </p>
-          <p className="py-6">Check out some of my work right here</p>
+          <p className="py-6">
+            Take a look at a selection of projects that I have developed
+            individually.
+          </p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src,code,demo }) => (
+          {portfolios.map(({ id, src, code, demo }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -52,12 +55,19 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105 object-fill mx-auto mt-5"
               />
               <div className="flex items-center justify-center">
-                
-                  <a href={code} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</a>
-                
-                
-                  <a href={demo} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</a>
-                
+                <a
+                  href={code}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
+                  Code
+                </a>
+
+                <a
+                  href={demo}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
+                  Demo
+                </a>
               </div>
             </div>
           ))}
